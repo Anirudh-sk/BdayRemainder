17a30b71689e8dfc37812b89c949d507a572d8fc
ani <amazinganirudhhere@gmail.com>
2023-02-08 23:51:58 +0530

    file name

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Navbar from "./components/Navbar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
